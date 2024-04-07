b2e7e505a2ff81e88697333ea84b3dbcb81b1f8e
bobbyhadz <opmftw@gmail.com>
2024-04-07 10:32:04 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+export default function App() {
+  const employees = [
+    {id: 1, name: 'Alice', country: 'Austria'},
+    {id: 2, name: 'Bob', country: 'Belgium'},
+    {id: 3, name: 'Carl', country: 'Canada'},
+  ];
+
+  const results = [];
+
+  // 👇️ Can use forEach outside of your JSX code
+  // if you need to call a function once for each array element
+  employees.forEach((employee, index) => {
+    results.push(
+      <div key={index}>
+        <h2>name: {employee.name}</h2>
+        <h2>country: {employee.country}</h2>
+
+        <hr />
+      </div>,
+    );
+  });
+
+  // 👇️ Or map() directly in your JSX code
+  return (
+    <div>
+      {employees.map((employee, index) => {
+        return (
+          <div key={index}>
+            <h2>name: {employee.name}</h2>
+            <h2>country: {employee.country}</h2>
+
+            <hr />
+          </div>
+        );
+      })}
+
+      <hr />
+      <hr />
+      <hr />
+
+      {results}
+    </div>
+  );
+}
